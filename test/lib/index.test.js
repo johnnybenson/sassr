a8bf9cc7801a46a46f5fe5f95bd54bc85555ac2d
Matt Hayes <mhayes@tumblr.com>
2015-08-12 16:16:26 -0700

    Add a way to test the module by compiling it inline, but it depends on document.

--- test/lib/index.test.js
+++ test/lib/index.test.js
@@ -10,14 +10,23 @@ var browserify = require('browserify');
 var index = require('../../index');
 var bundleString = fs.readFileSync(path.resolve(__dirname, '../mock/requires-css.bundle.js')).toString();
 
+function moduleFromString(string, filename) {
+    var Module = module.constructor;
+    var m = new Module();
+    m._compile(string, filename);
+    return m.exports;
+}
+
 describe('index', function() {
     var resultString;
+    var inlineModule;
 
     before(function(done) {
         var b = browserify('./test/mock/requires-css');
         b.transform(index);
         b.bundle(null, function(error, buffer) {
             resultString = buffer.toString();
+            inlineModule = moduleFromString(resultString, path.resolve('../mock/requires-css.inline.js'));
             done();
         });
     });
@@ -29,5 +38,10 @@ describe('index', function() {
         resultLines.forEach(function(line, i) {
             assert.equal(line, bundleLines[i]);
         });
+    });
+
+    it('should expose the right stuff', function() {
+        assert.isTrue(true);
+        console.log(JSON.stringify(inlineModule, null, 2));
     });
 });
